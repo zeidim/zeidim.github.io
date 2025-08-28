5065300fcfeda1cc5615d0c1919c00967360cd9f
zeidim <35118578+zeidim@users.noreply.github.com>
2025-08-28 20:45:20 +0000

    Deploying to gh-pages from @ zeidim/zeidim.github.io@37fbf86a50a27d541f04659fe73becccfe016399 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -515,7 +515,7 @@ ninja.data = [{
         title: 'ResearchGate',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.researchgate.net/profile/https://www.researchgate.net/profile/Mahdi-Zeidi/", "_blank");
+          window.open("https://www.researchgate.net/profile/Mahdi-Zeidi/", "_blank");
         },
       },{
         id: 'social-scholar',
