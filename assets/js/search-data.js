b0b7a3ad66fb56af61dda999856360c1523386e5
zeidim <mm.zeidi1@gmail.com>
2025-11-23 23:20:30 +0000

    Deploying to gh-pages from @ zeidim/zeidim.github.io@ac14650a6622f59d6201575fd8e748d208f1d1c8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "Publications by categories in reversed chronological order.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
